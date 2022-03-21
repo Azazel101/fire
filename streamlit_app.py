17dfd748fd72ce3e5774c6aa362e3057c984324e
Azazel101 <roman.slovak@gmail.com>
2022-03-21 09:31:24 +0100

    slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,12 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 """
 
 st.write("Toto je test.")
+
+saving_rate = st.slider(
+    "# of results",
+    min_value=0,
+    max_value=1,
+    step=0.01,
+    value=0.5,
+    help="You can choose the number of keywords/keyphrases to display. Between 0 and 1, default number is 0.5.",
+)
