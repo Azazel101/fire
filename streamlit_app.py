906b5f24cb65a07f4f58e47cc47d9d787067f6ac
Azazel101 <roman.slovak@gmail.com>
2022-03-22 10:24:31 +0100

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,8 +117,8 @@ st.write("You would make on average:", amount, "per year")
 st.write("Calculate the principle you need given some average yearly interest rate to make x income")
 st.write("Vypočítajte si princíp, ktorý potrebujete vzhľadom na nejakú priemernú ročnú úrokovú sadzbu, aby ste dosiahli x výšku príjmu za rok.")
 
-interest_rate_raw_2 = st.number_input("What is your averagte yearly interest rate percentage (%)?")
-amount_1 = st.number_input("What is the amount per year that you want to make from that interest rate ?")
+interest_rate_raw_2 = st.number_input("What is your averagte yearly interest rate percentage (%)?",value=4.0, step=0.1)
+amount_1 = st.number_input("What is the amount per year that you want to make from that interest rate ?",value=10000)
 
 principle = amount_1 / ((interest_rate_raw_2) / 100)
 st.write("You would need a princle of about", principle, "to have an annual income of", amount_1,"% interest yearly from your principle.")
@@ -126,8 +126,8 @@ st.write("You would need a princle of about", principle, "to have an annual inco
 st.write("Calculate the average yearly interest rate you need given some principle amount to make x income amount per year.")
 st.write("Vypočítajte priemernú ročnú úrokovú sadzbu, ktorú potrebujete vzhľadom na nejakú zásadnú sumu, aby ste dosiahli x výšku príjmu za rok.")
 
-principle_1 = st.number_input("What is your principle amount?")
-amount_2 = st.number_input("What is the amount per year that you want to make from that interest rate ?")
+principle_1 = st.number_input("What is your principle amount?",value=10000)
+amount_2 = st.number_input("What is the amount per year that you want to make from that interest rate ?",value=10000)
 
 interest_rate = amount_2 / principle_1 * 100
 
